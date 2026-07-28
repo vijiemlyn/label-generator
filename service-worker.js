4b4f49c05adc993afde8206bcfb883ec705927a2
vijiemlyn <133219469+vijiemlyn@users.noreply.github.com>
2026-07-28 23:47:48 +0530

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "tiny-genie-label-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
+});
+
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
